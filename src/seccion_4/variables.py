a790bb56ef6efaff257e58183e579371d4ed4714
Rodrigo <rodrigovelezdiaz@gmail.com>
2026-04-23 15:10:14 -0500

    Variables

---
+++ src/seccion_4/variables.py
@@ -0,0 +1,37 @@
+#Crear las variables: john, mary, y adam
+# Asignar valores a las variables. El valor debe de ser igual al número de manzanas que cada quien tenía
+
+john = 3
+mary = 5
+adam = 6
+
+#Una vez almacenados los números en las variables, imprimir las variables en una línea, y separar cada una de ellas con una coma
+
+print(john, mary, adam)
+
+#Después se debe crear una nueva variable llamada total_apples y se debe igualar a la suma de las tres variables anteriores
+
+total_apples = john + mary + adam
+
+#Imprime el valor almacenado en total_apples en la consola
+print(total_apples)
+
+
+#Experimenta con tu código: crea nuevas variables, asigna diferentes valores a ellas, y realiza varias operaciones aritméticas con ellas (por ejemplo, +, -, *, /, //, etc.). Intenta poner una cadena con un entero juntos en la misma línea, por ejemplo, "Número total de manzanas:" y total_apples
+
+juan = 8
+jose = 2
+luis = 13
+
+operacion1 = juan - jose
+operacion2 = luis * juan
+
+print(juan, jose, luis)
+print(operacion1)
+print(operacion2)
+
+print("Total de manzanas: ", total_apples)
+
+
+
+ 

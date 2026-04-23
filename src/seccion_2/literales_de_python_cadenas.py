e1a37724b7451681851373f72ea14615cf07d8b6
Oscar Alvarez <oscar43k45@gmail.com>
2026-04-22 20:29:02 -0500

    seccion2

---
+++ src/seccion_2/literales_de_python_cadenas.py
@@ -0,0 +1,4 @@
+#Escriba un fragmento de código de una línea, utilizando la función print(), así como los caracteres de nuevalínea y de escape, para que coincida con el resultado esperado que se muestra en la salida.
+
+print('"Estoy', "aprendiendo", sep='"""', end='"')
+print("Python", end='"""')

7648991ec5e2d41c4af05e49a771ce7ac7c7cc65
Oscar Alvarez <oscar43k45@gmail.com>
2026-04-22 19:14:06 -0500

    funcion_print

---
+++ src/seccion_1/funcion_print.py
@@ -0,0 +1,9 @@
+#Utiliza la función print() para imprimir la linea ¡Hola, Mundo! en la pantalla. Usa comillas dobles alrededor de la cadena.
+print("Hola Mundo!")
+
+#Habiendo hecho eso, usa la función print() nuevamente, pero esta vez imprime tu nombre.
+print('Rodrigo Velez') 
+print('Rodrigo Velez') 
+print('Rodrigo Velez') 
+print('Rodrigo Velez') 
+print('Rodrigo Velez')

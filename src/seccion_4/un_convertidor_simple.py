3f44da45e5e94c1c17eb67fc0572d101fb882d3f
Rodrigo <rodrigovelezdiaz@gmail.com>
2026-04-23 17:08:55 -0500

    Convertidor simple

---
+++ src/seccion_4/un_convertidor_simple.py
@@ -0,0 +1,17 @@
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers * 0.621371
+
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+dollars = 15
+euros = 8
+
+dollars_to_euros = dollars * 0.86
+euros_to_dollars = euros * 1.17
+
+print(dollars, "dolares son", round(dollars_to_euros, 2), "euros")
+print(euros, "euros son", round(euros_to_dollars, 2), "dolares")
